f3959b764e1517493e5d7e14c273b668dde9382e
Sean Grant <seangrant118@gmail.com>
2020-02-17 16:44:57 -0500

    app test changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+// import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+
+  expect(div.textContent).toContain('hi there')
+
+  ReactDOM.unmountComponentAtNode(div)
 });
