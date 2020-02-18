748c00fb9eb2129e0a23e286f5298b34d8896f82
Sean Grant <seangrant118@gmail.com>
2020-02-18 15:08:27 -0500

    enzyme setup

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -5,5 +5,8 @@ import App from "../App";
 it("shows a comment box", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+
+  expect(div.innerHTML).toContain('Comment Box')
+
   ReactDOM.unmountComponentAtNode(div);
 });
