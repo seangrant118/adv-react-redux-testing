073800a8e1e5e91a8ccfe9a394d115bcccb6dce7
Sean Grant <seangrant118@gmail.com>
2020-02-18 16:30:16 -0500

    before each added

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,12 +4,16 @@ import App from "components/App";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<App />);
+});
+
 it("shows a comment box", () => {
-  const wrapper = shallow(<App />);
   expect(wrapper.find(CommentBox).length).toEqual(1);
 });
 
 it("shows a comment list", () => {
-  const wrapper = shallow(<App />);
   expect(wrapper.find(CommentList).length).toEqual(1);
 });
