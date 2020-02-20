c5f36a19ba8ab1a3ce1f6581f07b5052662c45e8
Sean Grant <seangrant118@gmail.com>
2020-02-20 12:55:55 -0500

    describe statements

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,21 +17,21 @@ it("has a textarea and a button", () => {
   expect(wrapper.find("button").length).toEqual(1);
 });
 
-it("has a textarea that users can type in", () => {
-  wrapper.find("textarea").simulate("change", {
-    target: { value: "new comment" }
+describe("the text area", () => {
+  beforeEach(() => {
+    wrapper.find("textarea").simulate("change", {
+      target: { value: "new comment" }
+    });
+    wrapper.update();
   });
-  wrapper.update();
-  expect(wrapper.find("textarea").prop("value")).toBe("new comment");
-});
 
-it("empties textarea when form is submitted", () => {
-  wrapper.find("textarea").simulate("change", {
-    target: { value: "new comment" }
+  it("has a textarea that users can type in", () => {
+    expect(wrapper.find("textarea").prop("value")).toBe("new comment");
   });
-  wrapper.update();
-  expect(wrapper.find("textarea").prop("value")).toBe("new comment");
-  wrapper.find("form").simulate("submit");
-  wrapper.update();
-  expect(wrapper.find("textarea").prop("value")).toBe("");
+
+  it("empties textarea when form is submitted", () => {
+    wrapper.find("form").simulate("submit");
+    wrapper.update();
+    expect(wrapper.find("textarea").prop("value")).toBe("");
+  });
 });
