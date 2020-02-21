c1dcfb8e0239cc9c4b25819ba7062848ede1cc43
Sean Grant <seangrant118@gmail.com>
2020-02-21 14:48:52 -0500

    reducer test

---
+++ src/reducers/__test__/comments.test.js
@@ -0,0 +1,13 @@
+import commentsReducer from "reducers/comments";
+import { SAVE_COMMENT } from "actions/types";
+
+it("handles actions of type SAVE_COMMENT", () => {
+  const action = {
+    type: SAVE_COMMENT,
+    payload: "new comment"
+  };
+
+  const newState = commentsReducer([], action);
+
+  expect(newState).toEqual(["new comment"]);
+});
