f82fe3b6021faaf8c5d235f2f4d6619320a770b3
Sean Grant <seangrant118@gmail.com>
2020-02-22 12:36:32 -0500

    cheerio queries

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -16,4 +16,11 @@ beforeEach(() => {
   );
 });
 
-it("creates on li per comment", () => {});
+it("creates on li per comment", () => {
+  expect(wrapper.find("li").length).toEqual(2);
+});
+
+it("shows the text for each comment", () => {
+  expect(wrapper.render().text()).toContain("Comment 1");
+  expect(wrapper.render().text()).toContain("Comment 2");
+});
