cf0f2f29a933237349b00316bd3d22ae0b766c97
Sean Grant <seangrant118@gmail.com>
2020-02-25 14:38:49 -0500

    creating HOC

---
+++ src/components/requireAuth.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default ChildComponent => {
+  class ComposedComponent extends React.Component {
+    render() {
+      return <ChildComponent />;
+    }
+  }
+
+  return ComposedComponent;
+};
