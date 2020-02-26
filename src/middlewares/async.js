2af9931aff52c391aca9a3c25cdf127f86c7368a
Sean Grant <seangrant118@gmail.com>
2020-02-26 13:14:54 -0500

    state validator middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,8 +1,4 @@
 export default ({ dispatch }) => next => action => {
-  // Check to see if the action has a promise on payload property
-  // If it does, wait for it to resolve
-  // If it doesn't, send action to next middleware
-
   if (!action.payload || !action.payload.then) {
     return next(action);
   }
